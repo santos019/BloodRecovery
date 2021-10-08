387a2d41808d01cc5aaecdcf51357476497eeae3
EhitEpit <skcjddlf97@gmail.com>
2021-10-08 22:50:24 +0900

    CI/CD test2

--- src/main/java/com/longhair/bloodrecovery/HelloController.java
+++ src/main/java/com/longhair/bloodrecovery/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @GetMapping("/api/hello")
     public String hello(){
         return "안녕하세요. 현재 서버시간은 " + new Date() + "입니다.\n" +
-                "드디어 CI/CD 구축했어요~~~!!!";
+                "드디어 CI/CD 구축했어요~~~!!!으하하하하하";
     }
 }
