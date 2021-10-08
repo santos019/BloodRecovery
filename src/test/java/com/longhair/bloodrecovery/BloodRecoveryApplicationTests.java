de9cc344131accfc3f5df0083cac7d9c0177d33d
EhitEpit <skcjddlf97@gmail.com>
2021-10-08 18:12:28 +0900

    init commit

---
+++ src/test/java/com/longhair/bloodrecovery/BloodRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longhair.bloodrecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
