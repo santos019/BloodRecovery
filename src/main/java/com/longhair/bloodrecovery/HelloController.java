6babf8be64f1b64e5f5973cc63dc40455c1ca7ca
EhitEpit <skcjddlf97@gmail.com>
2021-10-19 09:13:11 +0900

    DirectDevelop Branch Test

--- src/main/java/com/longhair/bloodrecovery/HelloController.java
+++ src/main/java/com/longhair/bloodrecovery/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @GetMapping("/api/hello")
     public String hello(){
         return "안녕하세요. 현재 서버시간은 " + new Date() + "입니다.\n" +
-                "드디어 CI/CD 구축했어요~~~!!!자바 11버전 웹훅 테스트again";
+                "여긴 DirectDevelop Branch입니다~";
     }
 }
