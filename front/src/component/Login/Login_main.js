8633cf0b22307e42e1a9c6f63a447252dfdc0bef
santos019 <santos019@naver.com>
2021-11-01 12:36:35 +0900

    2021-11-01 12:36 메인화면 클릭 수정

--- front/src/component/Login/Login_main.js
+++ front/src/component/Login/Login_main.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import './Login_main.css';
 import Menu_nav_text from "../Common/Header/Menu_nav_text";
-function Login_main(){
+function Login_main() {
 
 
-    return(
-        <div className="Login_main_nav_class">
-          <Menu_nav_text name={"로그인"}></Menu_nav_text>
-          
+  return (
+    <div className="Login-main-class">
+      <div className="Login-main-container-class">
+        <Menu_nav_text name={"로그인"}></Menu_nav_text>
+      </div>
+      <div className="Login-main-input-container-class">
+        <div className="Login-main-input-id-class">
+          <input type="text" name="Login_id" >
+
+          </input>
+        </div>
+        <div className="Login-main-input-password-class">
+
         </div>
 
-    );
+      </div>
+      <div className="Login-main-input-btn-class">
+
+      </div>
+
+    </div>
+
+  );
 }
 
 export default Login_main;
