f76f4a1dde1d64528b026c0370a44d477a3ca842
santos019 <santos019@naver.com>
2021-11-01 20:25:51 +0900

    2021-11-01 20:25 지정헌혈 navbar 구현 완성

--- front/src/component/Directed/Directed_main.js
+++ front/src/component/Directed/Directed_main.js
@@ -1,22 +1,56 @@
 import React, { useState } from 'react';
-import Directed_nav from "./Directed_nav";
-import ReactModal from 'react-modal';
-import Main_Button from '../Common/Button/Main_Button';
-import UseLocalHook from '../Common/Function/UseLocalHook';
-function Directed_main (){
+import Menu_left_nav from '../Common/Header/Menu_left_nav';
+import './Directed_main.css';
+import DIRECTEDIMG from '../../Img/DIRECTEDIMG.png';
+import SEARCHICON from '../../Img/searchicon.png';
+import WRITEICON from '../../Img/WRITE.png';
 
-    const [modalIsOpen, setModalIsOpen] = UseLocalHook("true",false);
+const SelectBox = () => {
+	return (
+		<select>
+			<option key="ing" value="ing">
+				진행중
+			</option>
+			<option key="end" value="end">진행완료</option>
+		</select>
+	);
+};
+const SelectBox2 = () => {
+	return (
+		<select>
+			<option key="ing" value="ing">
+				서울
+			</option>
+			<option key="end" value="end">경기도</option>
+		</select>
+	);
+};
+
+
+function Directed_main() {
+
 
     return (
-        <div>
-               <div className="Directed_main_class" >
-           
-                  
-                    <Directed_nav></Directed_nav>
-                
-                
-        </div>
-           
+        <div className="Directed-main-container">
+            <div className="Directed-main-nav-container">
+                <div className="Directed-main-nav-class" >
+                    <Menu_left_nav name={"지정헌혈"} imgname={DIRECTEDIMG}></Menu_left_nav>
+                </div>
+                <div className="Directed-main-nav-search-class">
+                    <input type="text" name="search_Data" className="Directed-main-input">
+                    </input>
+                    <img src={SEARCHICON} className="Directed-main-nav-searchicon-class"></img>
+                </div>
+                <div className="Directed-main-nav-select-class">
+                    <SelectBox></SelectBox>
+                </div>
+                <div className="Directed-main-nav-select2-class">
+                    <SelectBox2></SelectBox2>
+                </div>
+                <div className="Directed-main-nav-write-class">
+                <img src={WRITEICON} className="Directed-main-nav-writeicon-class"></img>
+                </div>
+            </div>
         </div>
     )
 }
