149f3127e5f9207062249f34b8904f27075864b2
EhitEpit <skcjddlf97@gmail.com>
2021-11-03 20:33:39 +0900

    url 수정 및 CrossOrigin 추가

--- src/main/java/com/longhair/bloodrecovery/controller/UserController.java
+++ src/main/java/com/longhair/bloodrecovery/controller/UserController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
     @Autowired
     UserService userService;
