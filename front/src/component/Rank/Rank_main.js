2466d584d74635ff6b60b75c678397ce0e499692
gobong <chy9386@gmail.com>
2021-11-03 14:05:50 +0900

    민정아 시상대 이쁘게 해줘 고마워 하트

--- front/src/component/Rank/Rank_main.js
+++ front/src/component/Rank/Rank_main.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import Menu_nav_text from "../Common/Header/Menu_nav_text";
 import axios from "axios";
 import "./Rank_main.css";
+import gold from "../../Img/rank1.png";
+import silver from "../../Img/rank2.png";
+import bronze from "../../Img/rank3.png";
 
 function Rank_main() {
   const [ranks, setRanks] = useState([]);
@@ -12,17 +15,36 @@ function Rank_main() {
     });
   }, []);
 
+  const [my, setMyRank] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/rankings/1").then(function (response) {
+      setMyRank(response.data);
+    });
+  }, []);
+
+  const rankfunction = (ranking) => {
+    if (ranking === 1) return <img className="rank_1" src={gold}></img>;
+    else if (ranking === 2) return <img className="rank_1" src={silver}></img>;
+    else if (ranking === 3) return <img className="rank_1" src={bronze}></img>;
+  };
+
   return (
     <div>
       <Menu_nav_text name={"랭킹"}></Menu_nav_text>
 
       <div className="rank_my">
-        "nickname님의 포인트는 point점입니다. 현재 랭킹은 userRank위 입니다!"
+        {/* {my.map((myRank) => ( */}
+        <div>
+          "{my.userNickname}님의 포인트는 {my.userPoint}점입니다. 현재 랭킹은{" "}
+          {my.userRank}위 입니다!"
+        </div>
+        {/* ))} */}
       </div>
       <div className="rank_all">
         {ranks.map((rank) => (
           <div>
             {rank.userRank} {rank.userNickname} {rank.userPoint}
+            {rankfunction(rank.userRank)}
           </div>
         ))}
       </div>
@@ -33,5 +55,4 @@ function Rank_main() {
     </div>
   );
 }
-
 export default Rank_main;
