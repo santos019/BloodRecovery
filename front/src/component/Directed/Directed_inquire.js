da1009dff51a7faf6085512685962151841a6c89
santos019 <santos019@naver.com>
2021-11-06 09:32:23 +0900

    2021-11-06 09:32 메인 페이지 수정

---
+++ front/src/component/Directed/Directed_inquire.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Directed_inquire=()=>{
+
+
+
+    return(
+
+
+        <div className="Directed_inquire-container">
+
+            여기는 조회창
+        </div>
+    )
+
+}
+
+export default Directed_inquire;
--- front/src/component/Directed/Directed_main.js
+++ front/src/component/Directed/Directed_main.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Menu_left_nav from '../Common/Header/Menu_left_nav';
 import './Directed_main.css';
 import DIRECTEDIMG from '../../Img/DIRECTEDIMG.png';
 import SEARCHICON from '../../Img/searchicon.png';
 import WRITEICON from '../../Img/WRITE.png';
 import Directed_card from './Directed_card';
+import Directed_inquire from './Directed_inquire';
+
 import axios from "axios";
+
 
 const SelectBox = () => {
     return (
@@ -27,20 +31,26 @@ const SelectBox2 = () => {
         </select>
     );
 };
-const testcase=(response)=>{
 
-
-}
 
 //0관리자 1
-function Directed_main() {
+const Directed_main=(props)=>{
     var key1
     let form = new FormData();
     var arrNumber = new Array(); 
     const [getData, setGetdata] = useState([]);
+
+    const getsetValue = (props) => {
+        console.log("지정헌혈")
+        // props.getsetValue1("지정헌혈조회");
+     
+    }
+    const sendValue=(props)=>{
+        
+    }
     useEffect(() => {
         axios
-            .get("http://ec2-18-188-48-137.us-east-2.compute.amazonaws.com:8000/directeds/")
+            .get("http://ec2-18-219-208-124.us-east-2.compute.amazonaws.com:8000/direct")
         //     .data(
         //         {id: ,
         // requesterId: ,
@@ -72,7 +82,8 @@ function Directed_main() {
            // alert(JSON.stringify(getData.data));
         // .then(({ data }) => setRanks(data));
     }, []);
-
+  
+   
     const newdata = () => {
        for(var i=0; i<getData.length;i++)
        {
@@ -96,6 +107,8 @@ function Directed_main() {
         <div className="Directed-main-container">
             <div className="Directed-main-nav-container">
                 <div className="Directed-main-nav-class" >
+                <Link to="/test">test</Link>
+
                     <Menu_left_nav name={"지정헌혈"} imgname={DIRECTEDIMG}></Menu_left_nav>
                
                     {console.log("ge",getData[0]?.id)}
@@ -122,8 +135,8 @@ function Directed_main() {
                 {/* <Directed_card getData={getData}></Directed_card> */}
                 {/* {getData.map((menu)=>(menu.requesterId))
                 } */}
-                {
-                    getData.map((menu,index)=>(<Directed_card getData={getData[index]}  >
+                {//워닝이뜨기떄문에 key값설정해야함
+                    getData.map((menu,index)=>(<Directed_card getData={getData[index]} key={index} getsetValue={getsetValue} >
                         {console.log("index",index)}
                     </Directed_card>))
                 }
