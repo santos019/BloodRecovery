d957c00054254cbccd828c4af09d3a876795f469
1718390 <1718390@donga.ac.kr>
2021-11-09 17:07:41 +0900

    test

--- front/src/component/Mypage/Mypage_main.js
+++ front/src/component/Mypage/Mypage_main.js
@@ -1,14 +1,11 @@
 import React from "react";
-
-function Mypage_main(){
-
-
-    return(
-        <div>
-            mapage입니다.
-        </div>
-    );
-
+import Menu_left_nav from "../Common/Header/Menu_left_nav";
+function Mypage_main() {
+  return (
+    <div className="Mypage-main-container-class">
+      <Menu_left_nav name={"닉네임"}></Menu_left_nav>
+    </div>
+  );
 }
 
 export default Mypage_main;
