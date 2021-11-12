e70e23301b358472b462ac3ba58cf9f1fe2da829
Hyunyoung <86155410+gobongbong@users.noreply.github.com>
2021-11-12 16:47:18 +0900

    내가 쓴 글 조회 추가

--- src/main/java/com/longhair/bloodrecovery/repository/CardDonationRepository.java
+++ src/main/java/com/longhair/bloodrecovery/repository/CardDonationRepository.java
@@ -1,13 +1,16 @@
 package com.longhair.bloodrecovery.repository;
 
+import com.longhair.bloodrecovery.dto.CardRequestSimpleDto;
 import com.longhair.bloodrecovery.entity.CardRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CardDonationRepository extends JpaRepository<CardRequest, Long> {
     List<CardRequest> findCardRequestByCompleteStatus(boolean completeStatus);
+    List<CardRequest> findByUserId(String userId);
 }
 
